0e506194b46e23ffb7a473333420ff5dc4780ce8
LyricZhao <lyricz@yeah.net>
2019-10-17 12:06:08 +0800

    Add A_g size

---
+++ cluster_run.py
@@ -0,0 +1,33 @@
+import os
+import time
+import subprocess
+
+def generate_global_conf(model):
+    global_conf = ''
+    global_conf += 'JOB = {}\n'.format(model['JOB'])               ; print('[-]     JOB={}'.format(model['JOB']))
+    global_conf += 'basename = {}\n'.format(model['basename'])     ; print('[-]     basename={}'.format(model['basename']))
+    if not model['inputdir'].endswith('/'):
+        model['inputdir'] += '/'
+    if not model['outputdir'].endswith('/'):
+        model['outputdir'] += '/'        
+    global_conf += 'inputdir = {}\n'.format(model['inputdir'])     ; print('[-]     inputdir={}'.format(model['inputdir']))
+    global_conf += 'outputdir = {}\n'.format(model['outputdir'])   ; print('[-]     outputdir={}'.format(model['outputdir']))
+    global_conf += 'lowfreq = {}\n'.format(model['lowfreq'])       ; print('[-]     lowfreq={}'.format(model['lowfreq']))
+    global_conf += 'upfreq = {}\n'.format(model['upfreq'])         ; print('[-]     upfreq={}'.format(model['upfreq']))
+    global_conf += 'pOrder = {}\n'.format(model['pOrder'])         ; print('[-]     pOrder={}'.format(model['pOrder']))
+    return global_conf
+
+# Test configurations
+datasets = [
+    {'label': 'M1', 'JOB': 2, 'basename': 'Mtopo_6L_test.1', 'inputdir': 'models/input/Moon/M1', 'outputdir': 'models/output/Moon/M1', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M2', 'JOB': 2, 'basename': 'Mtopo_6L_1M.1'  , 'inputdir': 'models/input/Moon/M2', 'outputdir': 'models/output/Moon/M2', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M3', 'JOB': 2, 'basename': 'Mtopo_6L_2M.1'  , 'inputdir': 'models/input/Moon/M3', 'outputdir': 'models/output/Moon/M3', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M4', 'JOB': 2, 'basename': 'Mtopo_6L_test.1', 'inputdir': 'models/input/Moon/M4', 'outputdir': 'models/output/Moon/M4', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M5', 'JOB': 2, 'basename': 'Mtopo_6L_test.1', 'inputdir': 'models/input/Moon/M5', 'outputdir': 'models/output/Moon/M5', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M6', 'JOB': 2, 'basename': 'Mtopo_6L_test.1', 'inputdir': 'models/input/Moon/M6', 'outputdir': 'models/output/Moon/M6', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1}
+]
+nodes_list = [1, 2, 3, 4, 5, 6, 7, 8]
+ranks = 24
+threads = 1
+
+print('[N] Notes: this script must be run in demos dir, e.g.: python3 tools/cluster_run.py in demos dir')
--- parse_output.py
+++ parse_output.py
@@ -14,9 +14,9 @@ def parse_output(filename, print_result=True, model='', nodes=0, ranks=0):
     vertices = int(re.compile('of vertices:.*?(\d+)').search(st).group(1))
     threads = int(re.compile('check number of threads.*?(\d+)').search(st).group(1))
     deg = int(re.compile('polynomial deg.*?(\d+)').search(st).group(1))
-    Ad_size = A_size = Ap_size = 0
-    if st.find('total Ad size') != -1: Ad_size = int(re.compile('total Ad size.*?(\d+)').search(st).group(1))
-    if st.find('total A size')  != -1: A_size = int(re.compile('total A size.*?(\d+)').search(st).group(1))
+    Ag_size = A_size = Ap_size = 0
+    if st.find('total Ad size') != -1: Ag_size = int(re.compile('total Ad size.*?(\d+)').search(st).group(1))
+    # if st.find('total A size')  != -1: A_size = int(re.compile('total A size.*?(\d+)').search(st).group(1))
     if st.find('total Ap size') != -1: Ap_size = int(re.compile('total Ap size.*?(\d+)').search(st).group(1))
 
     lb = re.compile('lmin ([-+E.\d]+).*?lmax.*?([-+E.\d]+)', re.S).search(st)
@@ -40,8 +40,8 @@ def parse_output(filename, print_result=True, model='', nodes=0, ranks=0):
             print('-   Model: {}'.format(model))
             print('-   nn/np: {}/{}'.format(nodes, ranks))
         print('-     elm: {}'.format(elements))
-        if Ad_size: print('- Ad size: {}'.format(Ad_size))
-        if A_size : print('- A  size: {}'.format(A_size))
+        if Ag_size: print('- Ag size: {}'.format(Ag_size))
+        # if A_size : print('- A  size: {}'.format(A_size))
         if Ap_size: print('- Ap size: {}'.format(Ap_size))
         print('[WEAK SCALING] RESULTS:')
         print('-      Av: {:.6f}'.format(Av_time))
