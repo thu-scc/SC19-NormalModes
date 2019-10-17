79f99ca5df7530cd995b29fbf9a08334751ab429
LyricZhao <lyricz@yeah.net>
2019-10-17 11:41:27 +0800

    Add all info to the parsing

--- parse_output.py
+++ parse_output.py
@@ -1,40 +1,61 @@
 import re
 import sys
 
-def parse_output(filename):
+def parse_output(filename, print_result=True, model='', nodes=0, ranks=0):
     f = open(filename)
-    st = "".join(f.readlines())
+    st = ''.join(f.readlines())
 
-    print('Parsing results ... ', flush=True, end='')
-    meshname = re.compile("mesh name: (.*)").search(st).group(1)
-    mpi_ranks = int(re.compile("mpi_size.*?(\d+)").search(st).group(1))
-    lower_freq = float(re.compile("lower frequency in mHz.*?([\d.]+)").search(st).group(1))
-    upper_freq = float(re.compile("upper frequency in mHz.*?([\d.]+)").search(st).group(1))
-    elements = int(re.compile("of elements:.*?(\d+)").search(st).group(1))
-    vertices = int(re.compile("of vertices:.*?(\d+)").search(st).group(1))
-    threads = int(re.compile("check number of threads.*?(\d+)").search(st).group(1))
-    deg = int(re.compile("polynomial deg.*?(\d+)").search(st).group(1))
+    print('Parsing results of {} ... '.format(filename), flush=True, end='')
+    meshname = re.compile('mesh name: (.*)').search(st).group(1)
+    mpi_ranks = int(re.compile('mpi_size.*?(\d+)').search(st).group(1))
+    lower_freq = float(re.compile('lower frequency in mHz.*?([\d.]+)').search(st).group(1))
+    upper_freq = float(re.compile('upper frequency in mHz.*?([\d.]+)').search(st).group(1))
+    elements = int(re.compile('of elements:.*?(\d+)').search(st).group(1))
+    vertices = int(re.compile('of vertices:.*?(\d+)').search(st).group(1))
+    threads = int(re.compile('check number of threads.*?(\d+)').search(st).group(1))
+    deg = int(re.compile('polynomial deg.*?(\d+)').search(st).group(1))
+    Ad_size = A_size = Ap_size = 0
+    if st.find('total Ad size') != -1: Ad_size = int(re.compile('total Ad size.*?(\d+)').search(st).group(1))
+    if st.find('total A size')  != -1: A_size = int(re.compile('total A size.*?(\d+)').search(st).group(1))
+    if st.find('total Ap size') != -1: Ap_size = int(re.compile('total Ap size.*?(\d+)').search(st).group(1))
 
-    lb = re.compile("lmin ([-+E.\d]+).*?lmax.*?([-+E.\d]+)", re.S).search(st)
+    lb = re.compile('lmin ([-+E.\d]+).*?lmax.*?([-+E.\d]+)', re.S).search(st)
     lambda_min = float(lb.group(1))
     lambda_max = float(lb.group(2))
 
-    tot_time = float(re.compile("Iteration time.*?([\d.]+)").search(st).group(1))
-    it = int(re.compile("Pol\(A\)\*v.*?([\d.]+).*?([\d]+).*?([\d.]+)").search(st).group(2))
-    rev_M_v_time = float(re.compile("Solve with B.*?([\d.]+).*?([\d]+).*?([\d.]+)").search(st).group(3))
+    tot_time = float(re.compile('Iteration time.*?([\d.]+)').search(st).group(1))
+    it = int(re.compile('Pol\(A\)\*v.*?([\d.]+).*?([\d]+).*?([\d.]+)').search(st).group(2))
+    rev_M_v_time = float(re.compile('Solve with B.*?([\d.]+).*?([\d]+).*?([\d.]+)').search(st).group(3))
 
-    M_A_Times = re.compile("Matvec in ChebIter.*?([\d.]+).*?([\d]+).*?([\d.]+)").findall(st)
+    M_A_Times = re.compile('Matvec in ChebIter.*?([\d.]+).*?([\d]+).*?([\d.]+)').findall(st)
     Mv_time = float(M_A_Times[0][2])
     Av_time = float(M_A_Times[1][2])
 
-    num_eigs = int(re.compile("Row\D*?(\d+?)\D*?[\d.E+-]*?\D*?Transform", re.S).search(st).group(1))
+    num_eigs = int(re.compile('Row\D*?(\d+?)\D*?[\d.E+-]*?\D*?Transform', re.S).search(st).group(1))
 
     print('OK!', flush=True)
-    print('[FIGURE 7] RESULTS:')
-    print('-      Av: {:.6f}'.format(Av_time))
-    print('-      Mv: {:.6f}'.format(Mv_time))
-    print('- M^(-1)v: {:.6f}'.format(rev_M_v_time))
-    print('-   total: {:.6f}'.format(tot_time))
+    if print_result:
+        print('[MODEL] INFO:')
+        if nodes > 0:
+            print('-   Model: {}'.format(model))
+            print('-   nn/np: {}/{}'.format(nodes, ranks))
+        print('-     elm: {}'.format(elements))
+        if Ad_size: print('- Ad size: {}'.format(Ad_size))
+        if A_size : print('- A  size: {}'.format(A_size))
+        if Ap_size: print('- Ap size: {}'.format(Ap_size))
+        print('[WEAK SCALING] RESULTS:')
+        print('-      Av: {:.6f}'.format(Av_time))
+        print('-      Mv: {:.6f}'.format(Mv_time))
+        print('[FIXED INTERVAL] RESULTS:')
+        print('- (ln,lx): ({:.6f}, {:.6f})'.format(lambda_min, lambda_max))
+        print('- (lo,up): ({}, {})'.format(lower_freq, upper_freq))
+        print('- (dg,it): ({}, {})'.format(deg, it))
+        print('-   total: {:.6f}'.format(tot_time))
+        print('[STRONG SCALABILITY] RESULTS:')
+        print('-      Av: {:.6f}'.format(Av_time))
+        print('-      Mv: {:.6f}'.format(Mv_time))
+        print('-  rev_Mv: {:.6f}'.format(rev_M_v_time))
+        print('-   total: {:.6f}'.format(tot_time))
 
 if __name__ == '__main__':
     filename = sys.argv[1]
--- slurm_run.py
+++ slurm_run.py
@@ -2,79 +2,79 @@ import os
 import time
 import subprocess
 
-run_user = "zhaocg"
-log_dir = "logs"
-env_cmd = "source /home/zhaocg/intel_env.sh\ncd /home/zhaocg/SC19/NormalModes/demos\n"
-bin_path = "../bin/plmvcg_stampede2.out"
+run_user = 'zhaocg'
+log_dir = 'logs'
+env_cmd = 'source /home/zhaocg/intel_env.sh\ncd /home/zhaocg/SC19/NormalModes/demos\n'
+bin_path = '../bin/plmvcg_stampede2.out'
 
 def generate_bash(nodes, ranks_per_node, threads_per_rank, log_label):
-    bash = ""
-    bash += "#!/bin/bash\n"
-    bash += "#SBATCH -o {}/{}_{}_{}_{}.log\n".format(log_dir, log_label, nodes, ranks_per_node, threads_per_rank)
-    bash += "#SBATCH --nodes={}\n".format(nodes)
-    bash += "#SBATCH --job-name={}_{}_{}_{}\n\n".format(log_label, nodes, ranks_per_node, threads_per_rank)
-    bash += "export OMP_NUM_THREADS={}\n".format(threads_per_rank)
-    bash += "export I_MPI_PROCESS_MANAGER=mpd\n\n"
+    bash = ''
+    bash += '#!/bin/bash\n'
+    bash += '#SBATCH -o {}/{}_{}_{}_{}.log\n'.format(log_dir, log_label, nodes, ranks_per_node, threads_per_rank)
+    bash += '#SBATCH --nodes={}\n'.format(nodes)
+    bash += '#SBATCH --job-name={}_{}_{}_{}\n\n'.format(log_label, nodes, ranks_per_node, threads_per_rank)
+    bash += 'export OMP_NUM_THREADS={}\n'.format(threads_per_rank)
+    bash += 'export I_MPI_PROCESS_MANAGER=mpd\n\n'
     bash += env_cmd
-    bash += "mpirun -bootstrap slurm -n {} {}".format(nodes * ranks_per_node, bin_path)
+    bash += 'mpirun -bootstrap slurm -n {} {}'.format(nodes * ranks_per_node, bin_path)
     return bash
 
 def generate_global_conf(model):
-    global_conf = ""
-    global_conf += "JOB = {}\n".format(model["JOB"])               ; print("[-]     JOB={}".format(model["JOB"]))
-    global_conf += "basename = {}\n".format(model["basename"])     ; print("[-]     basename={}".format(model["basename"]))
-    if not model["inputdir"].endswith("/"):
-        model["inputdir"] += "/"
-    if not model["outputdir"].endswith("/"):
-        model["outputdir"] += "/"        
-    global_conf += "inputdir = {}\n".format(model["inputdir"])     ; print("[-]     inputdir={}".format(model["inputdir"]))
-    global_conf += "outputdir = {}\n".format(model["outputdir"])   ; print("[-]     outputdir={}".format(model["outputdir"]))
-    global_conf += "lowfreq = {}\n".format(model["lowfreq"])       ; print("[-]     lowfreq={}".format(model["lowfreq"]))
-    global_conf += "upfreq = {}\n".format(model["upfreq"])         ; print("[-]     upfreq={}".format(model["upfreq"]))
-    global_conf += "pOrder = {}\n".format(model["pOrder"])         ; print("[-]     pOrder={}".format(model["pOrder"]))
+    global_conf = ''
+    global_conf += 'JOB = {}\n'.format(model['JOB'])               ; print('[-]     JOB={}'.format(model['JOB']))
+    global_conf += 'basename = {}\n'.format(model['basename'])     ; print('[-]     basename={}'.format(model['basename']))
+    if not model['inputdir'].endswith('/'):
+        model['inputdir'] += '/'
+    if not model['outputdir'].endswith('/'):
+        model['outputdir'] += '/'        
+    global_conf += 'inputdir = {}\n'.format(model['inputdir'])     ; print('[-]     inputdir={}'.format(model['inputdir']))
+    global_conf += 'outputdir = {}\n'.format(model['outputdir'])   ; print('[-]     outputdir={}'.format(model['outputdir']))
+    global_conf += 'lowfreq = {}\n'.format(model['lowfreq'])       ; print('[-]     lowfreq={}'.format(model['lowfreq']))
+    global_conf += 'upfreq = {}\n'.format(model['upfreq'])         ; print('[-]     upfreq={}'.format(model['upfreq']))
+    global_conf += 'pOrder = {}\n'.format(model['pOrder'])         ; print('[-]     pOrder={}'.format(model['pOrder']))
     return global_conf
 
 # Test configurations
 datasets = [
-    {"label": "M1", "JOB": 2, "basename": "Mtopo_6L_test.1", "inputdir": "models/input/Moon/M1", "outputdir": "models/output/Moon/M1", "lowfreq": 0.2, "upfreq": 2.0, "pOrder": 1},
-    {"label": "M2", "JOB": 2, "basename": "Mtopo_6L_1M.1"  , "inputdir": "models/input/Moon/M2", "outputdir": "models/output/Moon/M2", "lowfreq": 0.2, "upfreq": 2.0, "pOrder": 1},
-    {"label": "M3", "JOB": 2, "basename": "Mtopo_6L_2M.1"  , "inputdir": "models/input/Moon/M3", "outputdir": "models/output/Moon/M3", "lowfreq": 0.2, "upfreq": 2.0, "pOrder": 1},
-    {"label": "M4", "JOB": 2, "basename": "Mtopo_6L_test.1", "inputdir": "models/input/Moon/M4", "outputdir": "models/output/Moon/M4", "lowfreq": 0.2, "upfreq": 2.0, "pOrder": 1},
-    {"label": "M5", "JOB": 2, "basename": "Mtopo_6L_test.1", "inputdir": "models/input/Moon/M5", "outputdir": "models/output/Moon/M5", "lowfreq": 0.2, "upfreq": 2.0, "pOrder": 1},
-    {"label": "M6", "JOB": 2, "basename": "Mtopo_6L_test.1", "inputdir": "models/input/Moon/M6", "outputdir": "models/output/Moon/M6", "lowfreq": 0.2, "upfreq": 2.0, "pOrder": 1}
+    {'label': 'M1', 'JOB': 2, 'basename': 'Mtopo_6L_test.1', 'inputdir': 'models/input/Moon/M1', 'outputdir': 'models/output/Moon/M1', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M2', 'JOB': 2, 'basename': 'Mtopo_6L_1M.1'  , 'inputdir': 'models/input/Moon/M2', 'outputdir': 'models/output/Moon/M2', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M3', 'JOB': 2, 'basename': 'Mtopo_6L_2M.1'  , 'inputdir': 'models/input/Moon/M3', 'outputdir': 'models/output/Moon/M3', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M4', 'JOB': 2, 'basename': 'Mtopo_6L_test.1', 'inputdir': 'models/input/Moon/M4', 'outputdir': 'models/output/Moon/M4', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M5', 'JOB': 2, 'basename': 'Mtopo_6L_test.1', 'inputdir': 'models/input/Moon/M5', 'outputdir': 'models/output/Moon/M5', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1},
+    {'label': 'M6', 'JOB': 2, 'basename': 'Mtopo_6L_test.1', 'inputdir': 'models/input/Moon/M6', 'outputdir': 'models/output/Moon/M6', 'lowfreq': 0.2, 'upfreq': 2.0, 'pOrder': 1}
 ]
 nodes_list = [1, 2, 3, 4, 5, 6, 7, 8]
 ranks = 24
 threads = 1
 
-print("[N] Notes: this script must be run in demos dir, e.g.: python3 tools/slurm_run.py in demos dir")
+print('[N] Notes: this script must be run in demos dir, e.g.: python3 tools/slurm_run.py in demos dir')
 for model in datasets:
-    print("[@] Preparing model: {}".format(model["label"]))
-    print("[-]   Global conf:")
+    print('[@] Preparing model: {}'.format(model['label']))
+    print('[-]   Global conf:')
     global_conf = generate_global_conf(model)
-    if not os.path.exists(model["inputdir"]):
-        print("[E]   Model does not exist, jump to next one")
+    if not os.path.exists(model['inputdir']):
+        print('[E]   Model does not exist, jump to next one')
         continue
-    if not os.path.exists(model["outputdir"]):
-        os.makedirs(model["outputdir"])
-        print("[I]   {} created".format(model["outputdir"]))
-    with open("global_conf", "w") as f:
+    if not os.path.exists(model['outputdir']):
+        os.makedirs(model['outputdir'])
+        print('[I]   {} created'.format(model['outputdir']))
+    with open('global_conf', 'w') as f:
         f.write(global_conf)
-        print("[I] global_conf written")
-    print("[-]   Beginning to run on {} nodes".format(nodes_list))
+        print('[I] global_conf written')
+    print('[-]   Beginning to run on {} nodes'.format(nodes_list))
     for nodes in nodes_list:
-        print("[!]     Nodes: {}".format(nodes))
-        print("[!]     Ranks per node: {}".format(ranks))
-        print("[!]     Threads per rank: {}".format(threads))
-        with open("slurm_generated_run.sh", "w") as f:
-            f.write(generate_bash(nodes, ranks, threads, model["label"]))
-        os.system("sbatch slurm_generated_run.sh")
-        print("[!]     Task submitted, sleep 5s")
+        print('[!]     Nodes: {}'.format(nodes))
+        print('[!]     Ranks per node: {}'.format(ranks))
+        print('[!]     Threads per rank: {}'.format(threads))
+        with open('slurm_generated_run.sh', 'w') as f:
+            f.write(generate_bash(nodes, ranks, threads, model['label']))
+        os.system('sbatch slurm_generated_run.sh')
+        print('[!]     Task submitted, sleep 5s')
         time.sleep(5)
     print('[!]   Waiting the tasks to be finished')
     while True:
         if subprocess.check_output(['squeue']).decode('utf-8').find(run_user) == -1:
-            print('[!]   Task finished: {}'.format(model["label"]))
+            print('[!]   Task finished: {}'.format(model['label']))
             break
         time.sleep(30)
 
